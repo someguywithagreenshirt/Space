cdbf2b93e7860deed2f9372f9f0cf184a41f13b5
Richard Campbell <someguywithagreenshirt@gmail.com>
2021-12-20 19:50:46 -0700

    Initial Commit

---
+++ Shared/SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceApp.swift
+//  Shared
+//
+//  Created by Richard Campbell on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
